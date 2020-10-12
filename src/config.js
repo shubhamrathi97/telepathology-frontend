2d22b719d24d751d16444f9d46d9d2dda82844b4
Shubham Rathi <shubham.rathi97@gmail.com>
2020-10-12 23:44:16 +0530

    API pointing to HEROKU API endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const config = {
-    'djangoServerURL': 'https://telepathology.ml/api',
+    'djangoServerURL': 'https://arcane-retreat-58418.herokuapp.com',
     'djangoLocalServerURL': 'http://localhost:8000',
 };
 
